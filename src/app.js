84dc0012b194bcfefb62c3879e6d0547f294127f
Saúl Belbey <belbeysaul@hotmail.com>
2024-07-11 11:53:38 -0300

    new config for test

--- src/app.js
+++ src/app.js
@@ -1,22 +1,31 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 
-import usersRouter from './routes/users.router.js';
-import petsRouter from './routes/pets.router.js';
-import adoptionsRouter from './routes/adoption.router.js';
-import sessionsRouter from './routes/sessions.router.js';
+import usersRouter from "./routes/users.router.js";
+import petsRouter from "./routes/pets.router.js";
+import adoptionsRouter from "./routes/adoption.router.js";
+import sessionsRouter from "./routes/sessions.router.js";
 
 const app = express();
-const PORT = process.env.PORT||8080;
-const connection = mongoose.connect(`URL DE MONGO`)
+
+console.log("🚀 ~ Puerto configurado en:", process.env.PORT);
+console.log("🚀 ~ Variable MONGO_URL:", process.env.MONGO_URL);
+const PORT = process.env.PORT || 8080;
+
+try {
+    const connection = await mongoose.connect(process.env.MONGO_URL);
+    console.log("🚀 ~ Conexión a MongoDB establecida");
+} catch (error) {
+    console.log("🚀 ~ Error al conectar a MongoDB:", error);
+}
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/users',usersRouter);
-app.use('/api/pets',petsRouter);
-app.use('/api/adoptions',adoptionsRouter);
-app.use('/api/sessions',sessionsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/pets", petsRouter);
+app.use("/api/adoptions", adoptionsRouter);
+app.use("/api/sessions", sessionsRouter);
 
-app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
